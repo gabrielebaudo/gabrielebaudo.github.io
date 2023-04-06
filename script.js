774ac1c5f616c31e687b52d478a12b880cb1616a
Gabriele Baudo <gabri_ele01@hotmail.it>
2023-04-06 17:13:59 +0200

    modified

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 /* TODO: inserite il codice JavaScript necessario a completare il MHW! */
 
+//MAIN
+const elementList = document.querySelectorAll(".choice-grid div");
+let freeList = [];
+let selectedList = [];
+setListen();
+
+//IMPOSTA GLI HANDLER PER TUTTE LE POSSIBILI RISPOSTE
+function setListen(){
+    for(const elem of elementList){
+        elem.addEventListener('click', onClick);
+        freeList.push(elem);
+    }
+}
+
+
 //HANDLER PER IL CLICK, CONTROLLA AD OGNI SELEZIONE SE IL TEST E COMPLETATO O NO
 function onClick(event){
-    const check_elem = event.currentTarget;
-    const div_elem = check_elem.parentNode;
+    const div_elem = event.currentTarget;
+    const check_elem = div_elem.querySelector(".checkbox");
 
+    //Controlliamo se esiste già un elemento nei selected associato a quella domanda, se si lo sostituiamo
+    for(const elem of selectedList){
+        if(elem.dataset.questionId === div_elem.dataset.questionId){
+            const index = selectedList.indexOf(elem);
+            selectedList.splice(index, 1);
+            freeList.push(elem);
+        }
+    }
+    selectedList.push(div_elem);
     div_elem.classList.add("selected");
+    if(div_elem.classList.contains("unselected"))
+        div_elem.classList.remove("unselected");
     check_elem.src = "/images/checked.png"
 
-    let elementList = document.querySelectorAll(".checkbox");
-    for(const elem of elementList){
-        if(elem.parentNode.dataset.questionId === div_elem.dataset.questionId){
-            if(elem !== check_elem){
-                elem.parentNode.classList.remove("selected");
-                elem.parentNode.classList.add("unselected"); 
-                elem.src = "/images/unchecked.png";
-            }
+    //Lo rimuoviamo dai free
+    const index = freeList.indexOf(div_elem);
+    freeList.splice(index, 1);
+
+    //Cambiamo la grafica a tutti i free aventi quel question id
+    for(const elem of freeList){
+        if(elem.dataset.questionId === div_elem.dataset.questionId){
+            if(div_elem.classList.contains("selected"))
+                elem.classList.remove("selected");
+            elem.classList.add("unselected");
+            const tick = elem.querySelector(".checkbox")
+            tick.src = "/images/unchecked.png";
         }
     }
 
@@ -24,81 +54,11 @@ function onClick(event){
     }
 }
 
-//RESTITUISCE LA PERSONALITA CORRETTA
-function personalityfinder(){
-    let checkedList = document.querySelectorAll(".selected");
-    const personality = {blep: 0, happy: 0, sleeping: 0, dopey: 0, burger: 0, cart: 0, nerd: 0, shy: 0, sleepy: 0}
-    for(const elem of checkedList){
-        for(const person in personality){
-            if(elem.dataset.choiceId === person){
-                personality[person]++;
-            }   
-        } 
-    }
-    let max = checkedList[0].dataset.choiceId;
-
-    for(const person in personality){
-        if(personality[person] > personality[max])
-            max = person;
-    }
-    return max;
-}
-
-//STAMPA TUTTI GLI ELEMENTI NECESSARI ALLA FINE DELLA PAGINA
-function display(personality){
-    console.log(RESULTS_MAP[personality].title);
-    const container = document.querySelector('#results');
-    const title = document.createElement("h1");
-    const description = document.createElement("p");
-    const button = document.createElement("div");
-
-    title.textContent = RESULTS_MAP[personality].title;
-    container.appendChild(title);
-    description.textContent = RESULTS_MAP[personality].contents;
-    container.appendChild(description);
-    button.textContent = "Ricomincia il quiz";
-    button.classList.add("button");
-    container.appendChild(button);
-}
-
 //CONTROLLA SE TUTTE LE DOMANDE HANNO AVUTO UNA RISPOSTA
 function isComplete(){
-    let checkedList = document.querySelectorAll(".selected");
-    if(checkedList.length === 3)
+    if(selectedList.length === 3)
         return 1;
     return 0;
-}
-
-//HANDLER PER IL BOTTONE DI RESET, PULISCE LA PAGINA E RICHIAMA GLI ONCLICK HANDLER
-function buttonPress(event){
-    const button = event.currentTarget;
-
-    let elementList = document.querySelectorAll(".checkbox");
-    for(const elem of elementList){
-        elem.parentNode.classList.remove("selected");
-        elem.parentNode.classList.remove("unselected"); 
-        elem.src = "/images/unchecked.png"
-    }
-
-    let result = document.querySelector("#results");
-    result.innerHTML = '';
-    setListen();
-}
-
-//IMPOSTA GLI HANDLER PER TUTTE LE POSSIBILI RISPOSTE
-function setListen(){
-    let elementList = document.querySelectorAll(".checkbox");
-    for(const elem of elementList){
-        elem.addEventListener('click', onClick);
-    }
-}
-
-//BLOCCA GLI HANDLER PER RENDERE LE RISPOSTE IMMODIFICABILI
-function blockListen(){
-    let elementList = document.querySelectorAll(".checkbox");
-    for(const elem of elementList){
-        elem.removeEventListener('click', onClick);
-    }
 }
 
 //GESTISCE TUTTE LE FUNZIONI PER LA CONCLUSIONE DEL TEST
@@ -113,18 +73,68 @@ function endTest(){
     button.addEventListener("click", buttonPress);
 }
 
-//MAIN
-setListen();
+//BLOCCA GLI HANDLER PER RENDERE LE RISPOSTE IMMODIFICABILI
+function blockListen(){
+    for(const elem of elementList){
+        elem.removeEventListener('click', onClick);
+    }
+}
+
+//RESTITUISCE LA PERSONALITA CORRETTA
+function personalityfinder(){
+    const personality = {blep: 0, happy: 0, sleeping: 0, dopey: 0, burger: 0, cart: 0, nerd: 0, shy: 0, sleepy: 0}
+    for(const elem of selectedList){
+        for(const person in personality){
+            if(elem.dataset.choiceId === person){
+                personality[person]++;
+            }   
+        } 
+    }
+    let max = selectedList[0].dataset.choiceId;
+
+    for(const person in personality){
+        if(personality[person] > personality[max])
+            max = person;
+    }
+    return max;
+}
+
+//STAMPA TUTTI GLI ELEMENTI NECESSARI ALLA FINE DELLA PAGINA
+function display(personality){
+    const container = document.getElementById('end');
+    container.classList.add("results");
+    const title = document.createElement("h1");
+    const description = document.createElement("p");
+    const button = document.createElement("div");
+
+    title.textContent = RESULTS_MAP[personality].title;
+    container.appendChild(title);
+    description.textContent = RESULTS_MAP[personality].contents;
+    container.appendChild(description);
+    button.textContent = "Ricomincia il quiz";
+    button.classList.add("button");
+    container.appendChild(button);
+}
 
 
+//HANDLER PER IL BOTTONE DI RESET, PULISCE LA PAGINA E RICHIAMA GLI ONCLICK HANDLER
+function buttonPress(event){
+    const button = event.currentTarget;
 
+    for(const elem of elementList){
+        elem.classList.remove("selected");
+        elem.classList.remove("unselected"); 
+        const tick = elem.querySelector(".checkbox")
+        tick.src = "/images/unchecked.png"
+    }
 
-
-
-
-
-
-
+    //svuotiamo la lista selected
+    selectedList = [];
+    let result = document.querySelector(".results");
+    result.innerHTML = '';
+    result.classList.remove("results");
+    setListen();
+}
 
 
 
